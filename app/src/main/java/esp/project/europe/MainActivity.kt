940dfdfbf72706d27a84b4d4ce502657e04de68a
giacomoazzari <giacomo.azzari@studenti.unipd.it>
2025-05-29 16:33:27 +0200

    Added dependencies for WindowManager. Added tabletop_layout.xml and its implementation on MainActivity.

--- app/src/main/java/esp/project/europe/MainActivity.kt
+++ app/src/main/java/esp/project/europe/MainActivity.kt
@@ -1,11 +1,41 @@
 package esp.project.europe
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.Lifecycle
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import kotlinx.coroutines.launch
+import androidx.window.layout.WindowLayoutInfo
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Avvia l'ascolto della postura dello schermo
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                WindowInfoTracker.getOrCreate(this@MainActivity)
+                    .windowLayoutInfo(this@MainActivity)
+                    .collect { layoutInfo ->
+                        if (isTabletopMode(layoutInfo)) {
+                            setContentView(R.layout.tabletop_layout)
+                        } else {
+                            setContentView(R.layout.activity_main)
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun isTabletopMode(layoutInfo: WindowLayoutInfo): Boolean {
+        return layoutInfo.displayFeatures.any { feature ->
+            feature is FoldingFeature &&
+                    feature.state == FoldingFeature.State.HALF_OPENED &&
+                    feature.orientation == FoldingFeature.Orientation.HORIZONTAL
+        }
     }
 }
